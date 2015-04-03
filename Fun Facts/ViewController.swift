79fb153f1063bf0c6e148430f2ec118fa90a53c6
Christine Rose <christinerose@christine-roses-macbook-pro.local>
2015-04-03 08:15:25 -0700

    Initial Commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Christine Rose on 4/3/15.
+//  Copyright (c) 2015 Christine Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
